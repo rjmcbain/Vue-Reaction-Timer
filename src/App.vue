603ce641ff1f08c2726111d7043551a749b175f5
rjmcbain <rmcbain01@gmail.com>
2021-03-01 12:43:19 -0800

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play!</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
+import Block from "./components/Block";
+import Results from "./components/Results";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Block,
+    Results,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.floor(Math.random() * 5000);
+      // console.log(this.delay);
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
 };
 </script>
 
